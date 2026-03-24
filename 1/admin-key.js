8730e0202864d8a40e5c2164a9883e5e7127f009
ting560 <160437906+ting560@users.noreply.github.com>
2026-03-23 22:07:29 -0300

    Delete 1 directory

--- 1/admin-key.js
+++
@@ -1,5 +0,0 @@
-// admin-key.js
-
-// **IMPORTANTE: Mude esta chave! Use uma senha longa e complexa.**
-// Esta chave será pedida na tela de login do Admin.
-export const ADMIN_KEY = "CrisTiano87492#2@&7";
